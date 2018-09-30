26b7ac7d32cb09590fd535a44475dcdff9d22fcc
Danny Wood <danwood76@gmail.com>
2018-09-30 13:03:57 +0100

    a3xelte: Add RIL configuration
    Change-Id: If39366fd746fb55efeeef906b3bdc64af17ecc9b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,10 @@ TARGET_OTA_ASSERT_DEVICE := a3xelte,a3xeltexx,a3xelteub,a3xeltedo
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
+# RIL
+BOARD_MODEM_TYPE := tss310
+BOARD_PROVIDES_LIBRIL := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_sec
 
