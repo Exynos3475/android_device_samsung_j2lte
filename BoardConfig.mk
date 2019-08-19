bebb96b1aa6d148c06ce7fb6971c8d46038877c9
TBM <32852493+TBM13@users.noreply.github.com>
2019-08-19 19:50:20 -0300

    Use proper kernel cross compile prefix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,7 @@ TARGET_KERNEL_HEADER_ARCH := arm
 TARGET_USES_UNCOMPRESSED_KERNEL := true
 
 # Kernel config
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 TARGET_KERNEL_SOURCE := kernel/samsung/exynos3475
 TARGET_KERNEL_CONFIG := lineage-o5lteswa_defconfig
