f131384e5016d824943304df5c2a6cac14697652
Griffin98 <griffin98@protonmail.com>
2019-11-16 11:14:15 -0300

    j2lte: build Bluetooth HAL

--- device.mk
+++ device.mk
@@ -28,6 +28,11 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_WIDTH := 540
 TARGET_SCREEN_HEIGHT := 960
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+		libbt-vendor
+
 # Flat device tree for boot image
 PRODUCT_PACKAGES += \
     dtbhtoolExynos
