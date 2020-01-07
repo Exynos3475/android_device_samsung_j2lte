a43207bb6029efe1e9938138aa946385167cdd42
Danny Wood <danwood76@gmail.com>
2020-01-07 10:22:57 +0000

    a3xelte: init: override ro.system.build.fingerprint to avoid android system warning on boot
    Change-Id: Ia720cef3d0cf8fdf1e957a79dbc68a47f76c1933

--- init/init_a3xelte.cpp
+++ init/init_a3xelte.cpp
@@ -88,24 +88,28 @@ void vendor_load_properties()
     if (bootloader.find("A310F") != std::string::npos) {
         /* SM-A310F */
         property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a3xeltexx/a3xelte:7.0/NRD90M/A310FXXU3CQL3:user/release-keys");
+        property_override("ro.system.build.fingerprint", "samsung/a3xeltexx/a3xelte:7.0/NRD90M/A310FXXU3CQL3:user/release-keys");
         property_override("ro.build.description", "a3xeltexx-user 7.0 NRD90M A310FXXU3CQL3 test-keys");
         property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-A310F");
         property_override_dual("ro.product.device", "ro.vendor.product.device", "a3xeltexx");
     } else if (bootloader.find("A310M") != std::string::npos) {
         /* SM-A310M */
         property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a3xelteub/a3xelte:7.0/NRD90M/A310MUBU2CQL5:user/release-keys");
+        property_override("ro.system.build.fingerprint", "samsung/a3xelteub/a3xelte:7.0/NRD90M/A310MUBU2CQL5:user/release-keys");
         property_override("ro.build.description", "a3xelteub-user 7.0 NRD90M A310MUBU2CQL5 release-keys");
         property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-A310M");
         property_override_dual("ro.product.device", "ro.vendor.product.device", "a3xelteub");
     } else if (bootloader.find("A310N0") != std::string::npos) {
         /* SM-A310N0 */
         property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a3xeltekx/a3xeltekx:7.0/NRD90M/A310N0KOU1CQL2:user/release-keys");
+        property_override("ro.system.build.fingerprint", "samsung/a3xeltekx/a3xeltekx:7.0/NRD90M/A310N0KOU1CQL2:user/release-keys");
         property_override("ro.build.description", "a3xeltekx-user 7.0 NRD90M A310N0KOU1CQL2 release-keys");
         property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-A310N0");
         property_override_dual("ro.product.device", "ro.vendor.product.device", "a3xeltekx");
     } else {
         /* SM-A310Y */
         property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a3xeltedo/a3xelte:7.0/NRD90M/A310YDVU3CQK1:user/release-keys");
+        property_override("ro.system.build.fingerprint", "samsung/a3xeltedo/a3xelte:7.0/NRD90M/A310YDVU3CQK1:user/release-keys");
         property_override("ro.build.description", "a3xeltedo-user 7.0 NRD90M A310YDVU3CQK1 release-keys");
         property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-A310Y");
         property_override_dual("ro.product.device", "ro.vendor.product.device", "a3xeltedo");
