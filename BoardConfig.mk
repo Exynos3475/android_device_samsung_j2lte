f7b8b330ae7575b20483008d87dd0621f2c64a55
TBM <mateo.trueba@gmail.com>
2020-01-16 12:09:10 -0300

    exynos3475: board: Use kernel zImage
    Partially reverts 48b75778152a4e26b43b92c156c9dd6a4c600282

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,7 @@ TARGET_CPU_VARIANT := cortex-a7
 # Extracted with libbootimg
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_SEPARATED_DT := true
