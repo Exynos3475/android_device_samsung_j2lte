0425c0af6cc8050f7cb847c066ca5b5f2b9c0ccd
Mateo Trueba <mateo.trueba@gmail.com>
2020-04-02 10:51:48 -0300

    Revert "j2lte: Switch to Exynos7580 Samsung audio HAL"
    This reverts commit a75dfc86c3db39a65dd29fa944737454d4073354.

--- device.mk
+++ device.mk
@@ -121,7 +121,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
     
 PRODUCT_PACKAGES += \
     audio.primary.universal3475 \
