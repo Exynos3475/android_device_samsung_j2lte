7fdb50721e6ad5f8d3f0c125ccc7e5ccb5cb50ae
Seden Ecem <ttbpccorporation@outlook.com>
2021-12-17 22:07:07 +0300

    j2lte: Update audio configs
    * Copy audio_effects.conf to system
    * audio_policy.conf synced with stock values, 7580 used as reference
    * Imported from j1xlte cm-14.1, edited for j2lte.
    * Weird audio issues (with OSS hal, such as muting etc.) mostly (or fully) gone, still needs more testing.
    * Basic USB-DAC adapters works up to 32bit/96kHz (nobody cares, even j1xlte released without otg support)

--- device.mk
+++ device.mk
@@ -116,6 +116,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
     
 PRODUCT_PACKAGES += \
